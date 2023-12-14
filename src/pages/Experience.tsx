ba30c73dbe38ec497f20869cc4276e1662cce76b
Felix Huang <felixhuang12@gmail.com>
2023-12-14 14:48:21 -0500

    Add tool tags for experiences

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,17 +1,18 @@
-import { Stack, Text, HStack } from "@chakra-ui/react"
+import { Stack, Text, HStack, Tag, Box } from "@chakra-ui/react"
 import Navbar from "../components/Navbar"
 
 interface ExperienceBlockProps {
     startAndEndDateText: string,
     jobTitle: string,
     company: string,
-    description: string
+    description: string,
+    tools: string[]
 }
 
 const ExperienceBlock = (props: ExperienceBlockProps) => {
     return (
         <HStack display={'flex'} justifyContent={'center'} alignItems={'flex-start'} spacing={8}
-        fontSize={{ base: 'xs', sm: 'md', md: 'l' }}>
+            fontSize={{ base: 'xs', sm: 'md', md: 'l' }}>
             <Text color={'grey'} minWidth={'20%'}>
                 {props.startAndEndDateText}
             </Text>
@@ -25,6 +26,11 @@ const ExperienceBlock = (props: ExperienceBlockProps) => {
                 <Text mt={3}>
                     {props.description}
                 </Text>
+                <Box display={'inline-block'}>
+                    {props.tools.map((tool) => (
+                        <Tag size={{ base: 'xs', sm: 'sm', md: 'md' }} key={tool} mt={4} mr={4} p={2} colorScheme={'teal'}>{tool}</Tag>
+                    ))}
+                </Box>
             </Stack>
         </HStack>
     )
@@ -34,13 +40,14 @@ const Experience = () => {
     return (
         <Stack display={'flex'} maxHeight={'100%'} maxWidth={'100%'} justifyContent={'center'}>
             <Navbar />
-            <Stack display={'flex'} maxWidth={'800px'} alignSelf={'center'} justifyContent={'center'} m={6} spacing={12}>
+            <Stack display={'flex'} maxWidth={'800px'} alignSelf={'center'} justifyContent={'center'} mx={8} mb={16} spacing={12}>
                 <ExperienceBlock
                     startAndEndDateText={"May — Aug 2023"}
                     jobTitle={"Software Development Engineer Intern"}
                     company={"Amazon Web Services"}
                     description={"Designed and developed a load test monitoring dashboard for Amazon Connect. \
                     Tracked and displayed the metrics outputted from load generated (agents, calls, chats, tasks, etc.) and outcome metrics."}
+                    tools={["AWS CDK", "AWS", "Java", "TypeScript"]}
                 />
                 <ExperienceBlock
                     startAndEndDateText={"Jun — Aug 2022"}
@@ -48,6 +55,7 @@ const Experience = () => {
                     company={"Fidelity Investments"}
                     description={"Worked with the Access and Accessibility team to engineer and improve accessiblity features for Fidelity's mobile platform \
                     and to support cryptocurrency offerings."}
+                    tools={["Swift", "SwiftUI", "Evinced SDK"]}
                 />
                 <ExperienceBlock
                     startAndEndDateText={"Sep 2020 — May 2022"}
@@ -55,6 +63,7 @@ const Experience = () => {
                     company={"Case Western Reserve University School of Medicine"}
                     description={"Conducted single-cell RNA-seq analysis to annotate transcription factor networks and expression differences among gene markers \
                     in cystic fibrosis-related epithelial cell lines. "}
+                    tools={["R", "Python", "Seurat"]}
                 />
             </Stack>
         </Stack>
