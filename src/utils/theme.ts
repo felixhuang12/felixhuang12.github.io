e8aaa68ed3475f1f5efee2a594f61d0926dfe803
Felix Huang <felixhuang12@gmail.com>
2023-12-14 01:41:20 -0500

    Add font

---
+++ src/utils/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    fonts: {
+        heading: `'Playfair Display', sans-serif`,
+        body: `'Playfair Display', sans-serif`,
+    },
+})
+
+export default theme
