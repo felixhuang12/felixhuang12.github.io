9f80f41467dcec585682a9f58d36de12b167b98d
Felix Huang <felixhuang12@gmail.com>
2023-12-25 01:13:21 -0500

    Remove react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './utils/theme';
+import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './utils/theme'
 import "@fontsource/playfair-display"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Home from './components/Home'
 
-function App() {
+const App = () => {
     return (
         <ChakraProvider theme={theme}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/experience" element={<Experience />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </Router>
+            <Home />
         </ChakraProvider>
     )
 }
 
-export default App;
+export default App
